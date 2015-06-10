dae75c5bcbfe0a5ff026d03db81f09cad37c8e8c
DiscSeven <linus.hedenberg@gmail.com>
2015-06-10 15:28:30 +0200

    Pollutantmean works for example output

--- pollutantmean.R
+++ pollutantmean.R
@@ -12,4 +12,17 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
   ## NOTE: Do not round the result!
+	
+	# Solution:
+	pollutants <- c() # Empty vector
+	for(i in id){
+		if(i<10) filename <- paste('00',i,'.csv', sep='')
+		else if(i<100) filename <- paste('0',i,'.csv', sep='')
+		else filename <- paste(i,'.csv', sep='')
+		currentdata <- read.csv(paste(directory,'/',filename, sep=''))
+		currentpollutant <- currentdata[, pollutant]
+		currentpollutant <- currentpollutant[!is.na(currentpollutant)]
+		pollutants <- c(pollutants, currentpollutant)
+	}
+	return(mean(pollutants))
 }
